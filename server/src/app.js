a0fea1e9220752d476d00f12247ba4c0a96b50e7
Mauro Bogado <mauroantoniobogado@gmail.com>
2021-06-25 21:41:19 -0300

    Agregado de botones y acciones

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 //agregamos el path
-const path = require('path');
-const multer = require('multer');
+//const path = require('path');
+//const multer = require('multer');
 
 const app = express()
 
 app.set('port', process.env.PORT || 4000);
 //agregado de las plantillas 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'))
+app.use(cors())
 app.use(morgan('dev'));
 //para que vea lo que he pedido
 app.use(express.json());
 //para entender los datos de los formularios
 app.use(express.urlencoded({ extended: false }));
-app.use(multer({ dest: path.join(__dirname, 'public/img/uploads') }).single('image'));
+//app.use(multer({ dest: path.join(__dirname, 'public/img/uploads') }).single('image'));
 //para no repetir las rutas
 app.use('/api/articles', require('./routes/articles.routes'))
 
